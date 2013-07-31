12e755df53467cd507d4a7263983dfbda4021095
Aaron Day <daya@crazydays.org>
2013-07-31 19:10:14 -0400

    Updated rendering to have join methods.

--- syntax.php
+++ syntax.php
@@ -64,31 +64,132 @@ class syntax_plugin_mobber
 
   private function render_head($mob)
   {
-    return  '<div class="row head">' .
-            $this->render_head_name($mob) .
-            $this->render_head_level_role($mob) .
-            $this->render_head_size_origin_type_keywords($mob) .
-            $this->render_head_xp($mob) . '</div>';
+    return
+      '<div class="row head">' .
+        $this->render_head_name($mob) .
+        $this->render_head_level_role($mob) .
+        $this->render_head_size_origin_type_keywords($mob) .
+        $this->render_head_xp($mob) .
+      '</div>';
   }
 
   private function render_head_name($mob)
   {
-    return '<div class="value half name">' . $mob->{'name'} . '</div>';
+    return
+      '<div class="value half name">' .
+        $this->join_name($mob) .
+      '</div>';
+  }
+
+  private function join_name($mob)
+  {
+    $name = '';
+    
+    if ($mob->{'name'}) {
+      $name .= ucwords($mob->{'name'});
+    }
+    
+    return $name;
   }
 
   private function render_head_level_role($mob)
   {
-    return '<div class="value half level_role">Level ' .
-           $mob->{'level'} . 'Elite Soldier (' . $mob->{'role'} . ')' . '</div>';
+    return
+      '<div class="value half level_role">' .
+        $this->join_level($mob) .
+        ' ' .
+        $this->join_role($mob) .
+      '</div>';
+  }
+
+  private function join_level($mob)
+  {
+    $level = '';
+    
+    if ($mob->{'level'}) {
+      $level .= 'Level ' . $mob->{'level'};
+    }
+    
+    return $level;
+  }
+
+  private function join_role($mob)
+  {
+    $role = '';
+
+    if ($mob->{'elite'}) {
+      $role .= 'Elite ';
+    }
+
+    if ($mob->{'solo'}) {
+      $role .= 'Solo ';
+    }
+    
+    if ($mob->{'role'}) {
+      $role .= ucwords($mob->{'role'});
+    }
+    
+    if ($mob->{'leader'}) {
+      $role .= '(Leader)';
+    }
+    
+    return $role;
   }
 
   private function render_head_size_origin_type_keywords($mob)
   {
-    return '<div class="value half size_origin_type_keywords">Huge shadow humanoid (undead)</div>';
+    return
+      '<div class="value half size_origin_type_keywords">' .
+        $this->join_size_origin_type_keywords($mob) .
+      '</div>';
+  }
+
+  private function join_size_origin_type_keywords($mob)
+  {
+    $joined = '';
+    
+    if ($mob->{'size'}) {
+      $joined .= ucwords($mob->{'size'});
+    }
+    
+    if ($mob->{'origin'}) {
+      $joined .= ucwords($mob->{'origin'});
+    }
+
+    if ($mob->{'type'}) {
+      $joined .= ucwords($mob->{'type'});
+    }
+
+    // if ($mob->{'keywords'} && count($mob->{'keywords'}) > 0) {
+    //   $joined .= '(';
+    //   for ($i = 0; $i < count($mob->{'keywords'}); $i++) {
+    //     if ($i > 0) {
+    //       $joined .= ', ';
+    //     }
+    //     $joined .= $mob->{'keywords'}[$i];
+    //   }
+    //   $joined .= ')';
+    // }
+    
+    return $joined;
   }
 
   private function render_head_xp($mob)
   {
-    return '<div class="value half xp">' . $mob->{'xp'} . '</div>';
+    return
+      '<div class="value half xp">' .
+        $this->join_xp($mob) .
+      '</div>';
+  }
+  
+  private function join_xp($mob)
+  {
+    $joined = '';
+    
+    if ($mob->{'xp'}) {
+      $joined .= $mob->{'xp'};
+    }
+    
+    return $joined;
   }
 }
